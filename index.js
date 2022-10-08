91f3e0bacf78d80ce85721a05c0429fccf1c1c6e
Ankit Kumar <>
2022-10-08 07:57:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//jshin esversion:6
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname +"/signup.html");
+});
+
+app.post("/",function(req,res){
+  var firstName=req.body.fName;
+  var LastName=req.body.lName;
+  var email=req.body.email;
+  console.log(firstName,LastName,email);
+});
+
+
+app.listen(3000,function(){
+  console.log("server is running on 3000");
+});
